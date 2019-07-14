08f5df6fdd525fa58ddef1d3f3c4c55ae08c0737
taksk <sakashitatk@me.com>
2019-07-14 15:46:42 +0900

    Add ABC133

---
+++ competition/AtCoder Beginner Contest 133/B - Good Distance/Main.java
@@ -0,0 +1,87 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	private static void solve(){
+		//Implement solution here.
+		int n = ni();
+		int d = ni();
+		int result = 0;
+		int[][] x = new int[n][d];
+		for(int i = 0; i < n; i++) {
+			x[i] = na(d);
+		}
+
+		for(int i = 0; i < n; i++) {
+			for(int j = i + 1; j < n; j++) {
+				double tmp = 0;
+				for(int k = 0; k < d; k++) {
+					tmp += Math.pow(x[i][k] - x[j][k], 2);
+				}
+				tmp = Math.pow(tmp, 0.5);
+				if((double)(int)tmp == tmp) {
+					result++;
+				}				
+			}			
+		}
+
+		System.out.println(result);
+	}
+
+	//Switch input source (stdin/file)
+	private static BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args){
+		String debugDataPath = System.getenv("DD_PATH");        
+		if(debugDataPath != null){
+			try{
+				br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(debugDataPath))));
+			}catch(Exception e){
+				throw new RuntimeException(e);
+			}
+		}
+		solve();
+	}
+
+	//Input read utility
+	private static StringTokenizer tokenizer = null;
+	public static String next() {
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+			try {
+				tokenizer = new StringTokenizer(br.readLine());
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return tokenizer.nextToken();
+	}
+	//Get next single int
+	private static int ni() {
+		return Integer.parseInt(next());
+	}
+	//Get next single long
+	private static long nl() {
+		return Long.parseLong(next());
+	}
+	//Get next single double
+	private static double nd() {
+		return Double.parseDouble(next());
+	}
+	//Get next int array from one line
+	private static int[] na(int n) {
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++)
+			a[i] = ni();
+		return a;
+	}
+	//Get next char array from one line
+	private static char[] ns() {
+		return next().toCharArray();
+	}
+	//Get next long array from one line
+	private static long[] nal(int n) {
+		long[] a = new long[n];
+		for (int i = 0; i < n; i++)
+			a[i] = nl();
+		return a;
+	}
+}
