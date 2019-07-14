2637881fc91260203e09a0700bcefab3b789747a
sakashitatk <sakashitatk@localhost>
2019-07-13 23:23:59 -0700

    Add ABC 133

---
+++ competition/AtCoder Beginner Contest 133/C - Remainder Minimization 2019/Main.java
@@ -0,0 +1,80 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	private static void solve(){
+		//Implement solution here.
+		int l = ni();
+		int r = ni();
+		r = Math.min(r, l + 2019);
+		int result = Integer.MAX_VALUE;
+		for(int i = l; i <= r; i++) {
+			for(int j = i + 1; j <= r; j++) {
+				result = Math.min(result, ((i % 2019) * (j % 2019) % 2019));
+				if(result == 0) {
+					System.out.println(result);
+					return;
+				}
+			}				
+		}
+
+		System.out.println(result);
+	}
+
+	//Switch input source (stdin/file)
+	private static BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args){
+		String debugDataPath = System.getenv("DD_PATH");        
+		if(debugDataPath != null){
+			try{
+				br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(debugDataPath))));
+			}catch(Exception e){
+				throw new RuntimeException(e);
+			}
+		}
+		solve();
+	}
+
+	//Input read utility
+	private static StringTokenizer tokenizer = null;
+	public static String next() {
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+			try {
+				tokenizer = new StringTokenizer(br.readLine());
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return tokenizer.nextToken();
+	}
+	//Get next single int
+	private static int ni() {
+		return Integer.parseInt(next());
+	}
+	//Get next single long
+	private static long nl() {
+		return Long.parseLong(next());
+	}
+	//Get next single double
+	private static double nd() {
+		return Double.parseDouble(next());
+	}
+	//Get next int array from one line
+	private static int[] na(int n) {
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++)
+			a[i] = ni();
+		return a;
+	}
+	//Get next char array from one line
+	private static char[] ns() {
+		return next().toCharArray();
+	}
+	//Get next long array from one line
+	private static long[] nal(int n) {
+		long[] a = new long[n];
+		for (int i = 0; i < n; i++)
+			a[i] = nl();
+		return a;
+	}
+}
