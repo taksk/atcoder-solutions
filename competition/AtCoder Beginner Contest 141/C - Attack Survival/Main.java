5ef6fdfd7a0b5eb425870dda4b3146aee1912a1c
--grobal <sakashitatk@me.com>
2019-09-15 22:36:17 +0900

    Add ABC141

---
+++ competition/AtCoder Beginner Contest 141/C - Attack Survival/Main.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+
+import java.io.*;
+
+public class Main {
+	private static void solve(){
+		//Implement solution here.
+		int n = ni();		
+		int k = ni();
+		int q = ni();
+		int[] a = na(q);
+		
+		int[]p = new int[n];
+
+		for(int ai:a) {
+			p[ai - 1]++;
+		}
+		for(int pi:p) {
+			if(pi + k - q > 0) {
+				System.out.println("Yes");
+			} else {
+				System.out.println("No");
+			}
+		}		
+	}
+
+	//Switch input source (stdin/file)
+	private static BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args){
+		String debugDataPath = System.getenv("DD_PATH");        
+		if(debugDataPath != null){
+			try{
+				br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(debugDataPath))));
+			}catch(Exception e){
+				throw new RuntimeException(e);
+			}
+		} 
+		solve();
+	}
+
+	//Input read utility
+	private static StringTokenizer tokenizer = null;
+	public static String next() {
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+			try {
+				tokenizer = new StringTokenizer(br.readLine());
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return tokenizer.nextToken();
+	}
+	//Get next single int
+	private static int ni() {
+		return Integer.parseInt(next());
+	}
+	//Get next single long
+	private static long nl() {
+		return Long.parseLong(next());
+	}
+	//Get next single double
+	private static double nd() {
+		return Double.parseDouble(next());
+	}
+	//Get next int array from one line
+	private static int[] na(int n) {
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++)
+			a[i] = ni();
+		return a;
+	}
+	//Get next char array from one line
+	private static char[] ns() {
+		return next().toCharArray();
+	}
+	//Get next long array from one line
+	private static long[] nal(int n) {
+		long[] a = new long[n];
+		for (int i = 0; i < n; i++)
+			a[i] = nl();
+		return a;
+	}
+}
