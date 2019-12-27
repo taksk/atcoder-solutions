00826b62827a8d114ed18735767c2dbedc560584
taksk <sakashitatk@me.com>
2019-12-27 19:53:30 +0900

    Add ABC 148

---
+++ competition/AtCoder Beginner Contest 148/A - Round One/Main.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	private static void solve(){
+		//Implement solution here.
+		boolean[] ba = new boolean[]{true,true,true};
+		ba[ni() - 1] = false;
+		ba[ni() - 1] = false;
+		for(int i = 0; i < 3; i++) {
+			if(ba[i])System.out.println(i+1);
+		}
+		
+	}
+	
+	//Switch input source (stdin/file)
+	private static Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args){
+		String debugDataPath = System.getenv("DD_PATH");        
+		if(debugDataPath != null){
+			try{
+				scanner = new Scanner(new BufferedReader(new InputStreamReader(new FileInputStream(new File(debugDataPath)))));
+			}catch(Exception e){
+				throw new RuntimeException(e);
+			}
+		}
+		solve();
+	}
+
+	//Input read utility
+	private static StringTokenizer tokenizer = null;
+	public static String next() {
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+			try {
+				tokenizer = new StringTokenizer(scanner.nextLine());
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return tokenizer.nextToken();
+	}
+	//Get next single int
+	private static int ni() {
+		return Integer.parseInt(next());
+	}
+	//Get next single long
+	private static long nl() {
+		return Long.parseLong(next());
+	}
+	//Get next single double
+	private static double nd() {
+		return Double.parseDouble(next());
+	}
+	//Get next int array from one line
+	private static int[] na(int n) {
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++)
+			a[i] = ni();
+		return a;
+	}
+	//Get next char array from one line
+	private static char[] ns() {
+		return next().toCharArray();
+	}
+	//Get next long array from one line
+	private static long[] nal(int n) {
+		long[] a = new long[n];
+		for (int i = 0; i < n; i++)
+			a[i] = nl();
+		return a;
+	}
+}
