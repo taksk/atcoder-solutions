9783d34ff1d920c0b0f62440b6211eb9e3a579c9
taksk <sakashitatk@me.com>
2020-06-28 01:02:11 +0900

    Add ABC 171

---
+++ competition/AtCoder Beginner Contest 171/C - One Quadrillion and One Dalmatians/Main.java
@@ -0,0 +1,103 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {	
+
+	private static void solve(){
+		//Implement solution here.
+		long N = nl();
+		StringBuilder sb = new StringBuilder();
+		while(N > 0) {
+//			System.out.println(N);
+			long rem = -1;
+			if(N % 26 == 0) {
+				rem = 26;
+				if(N == 26) {
+					N = 0;
+				} else {
+					N /= 26L;
+					N--;
+				}
+			} else {
+				rem = N % 26L;
+				N /= 26L;
+			}	
+			sb.insert(0,(char)(rem + 96L));
+			
+			
+		}
+		System.out.println(sb.toString());
+	}
+
+	//Switch input source (stdin/file)
+	private static BufferedReader br;
+	public static void main(String[] args){
+		br = new BufferedReader(new InputStreamReader(System.in));
+		String debugDataPath = System.getenv("DD_PATH");        
+		if(debugDataPath != null){
+			try{
+				br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(debugDataPath))));
+			}catch(Exception e){
+				throw new RuntimeException(e);
+			}
+		}
+		boolean hasNext = true;
+		while(hasNext){
+			solve();	
+			try{
+				br.mark(1);
+				if(br.read() > 0){
+					br.reset();			
+					System.out.println("---");
+				} else {
+					hasNext = false;
+				}
+			}catch(Exception e){
+				throw new RuntimeException(e);
+			} 
+		}
+	}
+
+	//Input read utility
+	private static StringTokenizer tokenizer = null;
+	public static String next() {
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+			try {
+				tokenizer = new StringTokenizer(br.readLine());
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return tokenizer.nextToken();
+	}
+	//Get next single int
+	private static int ni() {
+		return Integer.parseInt(next());
+	}
+	//Get next single long
+	private static long nl() {
+		return Long.parseLong(next());
+	}
+	//Get next single double
+	private static double nd() {
+		return Double.parseDouble(next());
+	}
+	//Get next int array from one line
+	private static int[] na(int n) {
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++)
+			a[i] = ni();
+		return a;
+	}
+	//Get next char array from one line
+	private static char[] ns() {
+		return next().toCharArray();
+	}
+	//Get next long array from one line
+	private static long[] nal(int n) {
+		long[] a = new long[n];
+		for (int i = 0; i < n; i++)
+			a[i] = nl();
+		return a;
+	}
+}
